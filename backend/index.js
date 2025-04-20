a001c1e1a0fcff0f413486ad3936fda176b79971
GulshanJha00 <gk4763549@gmail.com>
2025-04-20 22:55:22 +0530

    connection done from frontend to backend

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+var cors = require('cors')
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+
+io.on('connection', (socket) => {
+    socket.on('chatmessage', (msg) => {
+      console.log('message: ' + msg);
+    });
+});
+
+
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
+});
