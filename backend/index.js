6690fbc698e109fc8c325ecd4cd9926565ba9452
GulshanJha00 <gk4763549@gmail.com>
2025-04-24 03:03:51 +0530

    removed cors error

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: "https://cheated.vercel.app/",
+      origin: "https://cheated.vercel.app",
       methods: ["GET", "POST"],
     },
   });
 app.use(cors({
-    origin: "https://cheated.vercel.app/"
+    origin: "https://cheated.vercel.app"
 }))
 app.use(express.json()); 
 app.use(router)
